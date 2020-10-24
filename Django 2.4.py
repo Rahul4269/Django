4c898ec9428f84485ac9519b2877d680c7a38202
Rahul4269 <63596664+Rahul4269@users.noreply.github.com>
2020-10-24 19:16:40 +0530

    Add files via upload

---
+++ Django 2.4.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the first number"))
+
+b=int(input("Enter the second  number"))
+
+
+if (a and b )>=10:
+    def sum():
+        c=a+b
+        print(c)
+    sum()
+else:
+    print("numbers are less than 10")
+
+
